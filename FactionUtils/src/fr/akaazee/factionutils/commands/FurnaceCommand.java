862b81c4b37bd78db08a4e5a7400313bf197e551
Jules <jules.delobelle@free.fr>
2024-03-03 15:23:47 +0100

    Added limits on /furnace

--- FactionUtils/src/fr/akaazee/factionutils/commands/FurnaceCommand.java
+++ FactionUtils/src/fr/akaazee/factionutils/commands/FurnaceCommand.java
@@ -1,6 +1,9 @@
 package fr.akaazee.factionutils.commands;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -12,13 +15,44 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 
 public class FurnaceCommand implements CommandExecutor {
-
+	
+	private HashMap<UUID, Short> utilizations;
+	private LocalDate lastcheck;
+	
+	public FurnaceCommand() {
+		this.utilizations = new HashMap<UUID, Short>();
+		this.lastcheck = null;
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		
 	if(sender instanceof Player) {
 		Player player = (Player)sender;	
 		ItemStack[] items = player.getInventory().getContents();
+		if(isNewDay()) {
+			this.utilizations = new HashMap<UUID, Short>();
+		}
+		if(!this.utilizations.containsKey(player.getUniqueId())) {
+			
+			this.utilizations.put(player.getUniqueId(), (short) 4);
+			this.furnace(items, player);
+			player.sendMessage("§cIl vous reste §l4 utilisations§r§c de cette commande aujourd'hui");
+			
+		}else if (this.utilizations.get(player.getUniqueId()) > 0) {
+			
+			this.utilizations.replace(player.getUniqueId(), (short) (this.utilizations.get(player.getUniqueId())-1));
+			this.furnace(items, player);
+			player.sendMessage("§cIl vous reste §l" + this.utilizations.get(player.getUniqueId()) + " utilisations§r§c de cette commande aujourd'hui");
+		}else {
+			player.sendMessage("§cVous n'avez plus d'utillisations de cette commande aujourd'hui");
+		}
+		return true;
+	}
+	return false;
+	}
+	
+	private void furnace(ItemStack[] items, Player player) {
 		
 		for(int i = 0; i < items.length; i++) {
 			ItemStack result = null;
@@ -37,9 +71,13 @@ public class FurnaceCommand implements CommandExecutor {
 				}
 			}
 		}
-		return true;
-		}
-	return false;
+	}
+	
+	public boolean isNewDay() {
+	  LocalDate today = LocalDate.now();
+	  boolean ret = this.lastcheck == null || today.isAfter(this.lastcheck);
+	  this.lastcheck = today;
+	  return ret;
 	}
 
 }
